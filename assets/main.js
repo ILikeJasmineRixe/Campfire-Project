29f7ef106404e88a1194e9d08895cec468105ac5
ILikeJasmineRixe <ilikejasminerice@gmail.com>
2026-02-28 18:58:32 -0600

    introducing game functionality

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+const scanBtn = document.getElementById("scanBtn");
+const backend = document.getElementById("backend");
+const closeBackend = document.getElementById("closeBackend");
+
+// const canvas = document.getElementById("game");
+// const ctx = canvas.getContext("2d");
+
+let hp = 100;
+let score = 0;
+let bugs = [];
+
+const puzzles = []
